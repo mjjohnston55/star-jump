a6339973a231cfdc4a55e9c85c4235c3c37a95ce
Thomas Nelson <thnlsn@gmail.com>
2020-01-09 23:11:29 -0800

    updated contacts to stars

--- routes/stars.js
+++ routes/stars.js
@@ -7,28 +7,28 @@ const router = express.Router();
 // @desc        Get all users contacts (NOT ALL CONTACTS ON THE DATABASE)
 // @access      Private
 router.get('/', (req, res) => {
-  res.send('Get all users contacts');
+  res.send('Get all users stars');
 });
 
 // @route       POST api/contacts
 // @desc        Add new contacts
 // @access      Private
 router.post('/', (req, res) => {
-  res.send('Add contact');
+  res.send('Add star');
 });
 
 // @route       GET api/contacts/:id
 // @desc        Get all users contacts
 // @access      Private
 router.put('/:id', (req, res) => {
-  res.send('Update contact');
+  res.send('Update stars');
 });
 
 // @route       DELETE api/contacts
 // @desc        Delete contact
 // @access      Private
 router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+  res.send('Delete stars');
 });
 
 module.exports = router;
