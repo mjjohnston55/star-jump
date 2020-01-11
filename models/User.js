54e4ce56c97abb1e3502d5b75c7c42073916dc95
Thomas Nelson <thnlsn@gmail.com>
2020-01-11 02:52:15 -0800

    finished route for updating stars (api should be done)

--- models/User.js
+++ models/User.js
@@ -14,6 +14,11 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  stars: {
+    type: Number,
+    required: true,
+    default: 0
+  },
   date: {
     type: Date,
     default: Date.now
