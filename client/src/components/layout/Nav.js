cb6018ab43bf61f7b4e7feb2143f434ffe40f321
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-22 01:09:27 -0800

    added links for register and login to navbar ONLY for if a user isnt logged in

--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import '../../App.css';
 import { Link } from 'react-router-dom';
+import UserContext from '../../context/user/userContext';
 
 function Nav() {
+    const userContext = useContext(UserContext);
+
+    const { isAuthenticated, logout, user } = userContext;
+
+    const authLinks = (
+        <Fragment>
+            <li>Hello {user && user.name}</li>
+            <li>
+                <a href='#!'>
+                    <i className='fas fa-sign-out-alt'></i>{' '}
+                    <span className='hide-sm'>Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    ); // if there is a user, display the users name
+
+    const guestLinks = (
+        <Fragment>
+            <li>
+                <Link to='/register'>Register</Link>
+            </li>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+        </Fragment>
+    ); // if there is a user, display the users name
+
     const navStyle = {
         color: 'white'
     };
 
     return (
-        <nav>
+        <nav className='navbar bg-primary'>
             <Link to='/'>
-                <h1 style={navStyle}>Home</h1>
+                <h1 style={navStyle}>
+                    <i className='fas fa-graduation-cap'></i> Home
+                </h1>
             </Link>
             <ul className='nav-links'>
-                <Link style={navStyle} to='/mainapp'>
+                {isAuthenticated ? authLinks : guestLinks}
+                {/*                 <Link style={navStyle} to='/mainapp'>
                     <li>Main App</li>
-                </Link>
+                </Link> */}
             </ul>
         </nav>
     );
