1098a4bb74b688147484760b51fc905d8c2da125
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-25 10:32:50 -0800

    final

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // GET home route
-app.get('/', (req, res) => res.json('Welcome to the Kids-Learning-App API'));
+/* app.get('/', (req, res) => res.json('Welcome to the Kids-Learning-App API')); */
 
 ////// Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/stars', require('./routes/stars'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 // If there is an environment variable for hosted port, use it, otherwise use 5000 (development)
 const PORT = process.env.PORT || 8000;
