329601ed6e3470ebc706106ddbb9957d1a73d41f
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-01-27 15:55:29 -0800

    pushing group edits

--- client/src/components/gameModules/ABCApp.js
+++ client/src/components/gameModules/ABCApp.js
@@ -1,71 +1,94 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from "react";
 // import '../../App.css';
-import { Link } from 'react-router-dom';
-import UserContext from '../../context/user/userContext'; //////
-import Word from '../spellingComponents/Word'
-import WordBank from '../spellingComponents/WordBank'
-import Picture from '../spellingComponents/Picture'
-import LetterButton from '../spellingComponents/LetterButtons'
-import swal from 'sweetalert'
-import '../spellingComponents/spellingApp.css'
+import { Link } from "react-router-dom";
+import UserContext from "../../context/user/userContext"; //////
+import Word from "../spellingComponents/Word";
+import WordBank from "../spellingComponents/WordBank";
+import Picture from "../spellingComponents/Picture";
+import LetterButton from "../spellingComponents/LetterButtons";
+import swal from "sweetalert";
+import "../spellingComponents/spellingApp.css";
 
 const ABCApp = props => {
-    const [score, setScore] = useState(0)
-    const [word, setWord] = useState(WordBank.first())
-    const [hiddenLetterIndex, setHiddenLetterIndex] = useState(Math.floor(Math.random() * 3))
-    const [correctButtonIndex, setCorrectButtonIndex] = useState(Math.floor(Math.random() * 3))
-    const userContext = useContext(UserContext);
-    const { updateStars, user } = userContext;
-  
-    useEffect(() => {
-      setScore(score)
-      setWord(word)
-      setHiddenLetterIndex(hiddenLetterIndex)
-      setCorrectButtonIndex(correctButtonIndex)
-    }, [score, word, hiddenLetterIndex, correctButtonIndex])
-  
-      const correctGuess = () => {
-      // let { score } = this.state
-      // let { hiddenLetterIndex } = this.state
-      setScore(score + 1)
-      if (score > 10) {
-        swal("You won! You've earned the star for this game!")
-        updateStars(user, 3)
-        // props.history.push("/");
-        setTimeout(function() {
-          props.history.push("/");
-        }, 1500);
-      }
-      console.log(score)
-      setWord(WordBank.next(word))
-      setCorrectButtonIndex(Math.floor(Math.random() * 3))
-      setHiddenLetterIndex(Math.floor(Math.random() * word.length))
-    
-      console.log("CORRECT GUESS")
-    }
-  
-      // main render fuction 
-      return (
-        <div className="banner"> Fill in the missing letters in each word!
-        <div className="grid-container">
-            <Link to='/'>
-                <div className='back-arrow'></div>
-            </Link>
-          <Picture word={word}/>
-          <Word word={word} hiddenLetterIndex={hiddenLetterIndex} />
-          <div className="item3">
-          <LetterButton onClickEvent={correctGuess} buttonIndex={0} correctButtonIndex={correctButtonIndex} hiddenLetterIndex={hiddenLetterIndex} word={word}/>
-          <LetterButton onClickEvent={correctGuess} buttonIndex={1} correctButtonIndex={correctButtonIndex} hiddenLetterIndex={hiddenLetterIndex} word={word}/>
-          <LetterButton onClickEvent={correctGuess} buttonIndex={2} correctButtonIndex={correctButtonIndex} hiddenLetterIndex={hiddenLetterIndex} word={word}/> 
-          </div>
-        </div>
-        </div>
-      )
-    }
-  
-    export default ABCApp;
+  const [score, setScore] = useState(0);
+  const [word, setWord] = useState(WordBank.first());
+  const [hiddenLetterIndex, setHiddenLetterIndex] = useState(
+    Math.floor(Math.random() * 3)
+  );
+  const [correctButtonIndex, setCorrectButtonIndex] = useState(
+    Math.floor(Math.random() * 3)
+  );
+  const userContext = useContext(UserContext);
+  const { updateStars, user } = userContext;
 
+  useEffect(() => {
+    setScore(score);
+    setWord(word);
+    setHiddenLetterIndex(hiddenLetterIndex);
+    setCorrectButtonIndex(correctButtonIndex);
+  }, [score, word, hiddenLetterIndex, correctButtonIndex]);
 
+  const correctGuess = () => {
+    // let { score } = this.state
+    // let { hiddenLetterIndex } = this.state
+    setScore(score + 1);
+    if (score > 10) {
+      swal("You won! You've earned the star for this game!");
+      updateStars(user, 3);
+      // props.history.push("/");
+      setTimeout(function() {
+        props.history.push("/");
+      }, 1500);
+    }
+    console.log(score);
+    setWord(WordBank.next(word));
+    setCorrectButtonIndex(Math.floor(Math.random() * 3));
+    setHiddenLetterIndex(Math.floor(Math.random() * word.length));
+
+    console.log("CORRECT GUESS");
+  };
+
+  // main render fuction
+  return (
+    <div>
+      <Link to="/">
+        <div className="back-arrow"></div>
+      </Link>
+
+      <div className="banner"> Fill in the missing letters in each word!</div>
+      
+      <div className="grid-container">
+        <Picture word={word} />
+        <Word word={word} hiddenLetterIndex={hiddenLetterIndex} />
+        <div className="item3">
+          <LetterButton
+            onClickEvent={correctGuess}
+            buttonIndex={0}
+            correctButtonIndex={correctButtonIndex}
+            hiddenLetterIndex={hiddenLetterIndex}
+            word={word}
+          />
+          <LetterButton
+            onClickEvent={correctGuess}
+            buttonIndex={1}
+            correctButtonIndex={correctButtonIndex}
+            hiddenLetterIndex={hiddenLetterIndex}
+            word={word}
+          />
+          <LetterButton
+            onClickEvent={correctGuess}
+            buttonIndex={2}
+            correctButtonIndex={correctButtonIndex}
+            hiddenLetterIndex={hiddenLetterIndex}
+            word={word}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ABCApp;
 
 // function ABCApp() {
 //     const userContext = useContext(UserContext); //////
--- client/src/components/gameModules/ClockApp.js
+++ client/src/components/gameModules/ClockApp.js
@@ -109,14 +109,20 @@ function SightWordApp(props) {
           pickedWord +
           "--_us_1.mp3";
         if (score === 10) {
-          swal("You earned 3 stars!", "Great Job!", "success");
-          updateStars(user, 3)
-        // props.history.push("/");
+          if (isAuthenticated) {
+            swal('You earned 3 stars!', 'Great Job!', 'success');
+            updateStars(user, 3);
+        } else {
+            swal(
+                'You won! Make sure to login if you want to earn stars!',
+                'Great Job!',
+                'success'
+            );
+        }
         setTimeout(function() {
-          props.history.push("/");
+            props.history.push('/');
         }, 1500);
-          
-          return;
+        return;
         }
       }
       
