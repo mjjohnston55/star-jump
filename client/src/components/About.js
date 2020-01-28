b7a2a9c41bde11da3450ebdb01fa8a2c7324f8d9
Joseph Badua <52088997+JosephBadua@users.noreply.github.com>
2020-01-27 17:35:47 -0800

    about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css"
+import "../About.css"
 // import { Link } from 'react-router-dom';
 
 function About() {
@@ -12,6 +13,47 @@ function About() {
           <div className="col-lg-2"></div>
           <div className="col-lg-8 about"><p>Star Jump is an online application that takes a holistic approach to cultivate young learning habits through bright and engaging easy-to-start, fun-to-play games; encouraging them to continue through a reward-based system with “stars” and keeping persistent track of their progress through user logins.</p></div>
           <div className="col-lg-2"></div>
+        </div>
+      </div>
+      <br></br>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-4 about-section">
+          <div className="card-container">
+          <div className="flipper">
+            <div className="front">
+            <h1 className="topText">Play</h1>
+            </div>
+            <div className="back">
+            <h2 className="cardBack">Designed for young children, Star Jump's games are designed to make learning fun and enganging. Using bright colors and pictures. Your child is able to learn new words, practice their addition and subtraction and even learn about the solor system! Our games also include audio help our games accessible to all kids!</h2>
+            </div>
+          </div>
+        </div>
+          </div>
+          <div className="col-lg-4 about-section">
+          <div className="card-container">
+          <div className="flipper">
+            <div className="front">
+            <h1 className="topText">Learn</h1>      
+            </div>
+            <div className="back">
+            <h2 className="cardBack">As Star Jump grows, so does the range of our games and lessons. Our curriculum aims to cover multiple lessons for multiple grade levels, making children of all ages able to use Star Jump as a place to supplement their learning!</h2>
+            </div>
+          </div>
+        </div>    
+            </div>
+          <div className="col-lg-4 about-section">
+          <div className="card-container">
+          <div className="flipper">
+            <div className="front">
+            <h1 className="topText">Earn</h1>     
+            </div>
+            <div className="back">
+            <h2 className="cardBack">We want to make your child's learning experience fun, so we've made a reward system to encourage them to keep on playing and learning. When they complete a game, they earn "Stars" which can be used to track your child's progress and give you an idea of how they're doing. However, Stars can only be tracked if your child has a Star Jump account. </h2>
+            </div>
+          </div>
+        </div>
+          </div>
         </div>
       </div>
     </div>
