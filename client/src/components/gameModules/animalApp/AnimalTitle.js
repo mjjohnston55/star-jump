d001833d9c534e5038220a62a19e21e0a2a8e543
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-28 11:31:21 -0800

    styling

--- client/src/components/gameModules/animalApp/AnimalTitle.js
+++ client/src/components/gameModules/animalApp/AnimalTitle.js
@@ -5,15 +5,7 @@ const Title = ({ title, message, score, topScore }) => {
     return (
         <Fragment>
             <div className='title'>
-                {/*                 <img className='title-image' src={title} alt='Title' /> */}
                 <h1 className='title-text'>{title}</h1>
-            </div>
-            <div className='row message'>
-                <div className='col-lg-3 score'>Score: {score}</div>
-                <div className='col-lg-6'>
-                    <h3>{message}</h3>
-                </div>
-                <div className='col-lg-3 score'>Highscore: {topScore}</div>
             </div>
         </Fragment>
     );
